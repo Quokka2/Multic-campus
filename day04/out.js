1af482cafcf22bee7017681b6108533887a5af13
ColdBrew <99588377+Quokka2@users.noreply.github.com>
2023-02-02 17:34:36 +0900

    Add files via upload

---
+++ day04/out.js
@@ -0,0 +1,15 @@
+function 빨래하다(){
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물에 집어 넣는다.")
+    console.log("3. 비누를 바른다.")
+    console.log("4. 멱을 감는다.")
+    console.log("5. 물에 여러번 헹군다.")
+    console.log("6. 탈수.")
+    console.log("7. 건조.")
+ }
+ function 청소(){
+    console.log("1. 청소기.")
+    console.log("2. 정전기 패드.")
+    console.log("3. 물걸레.")
+    console.log("4. 건조.")    
+ }
---
+++ day04/out2.js
@@ -0,0 +1,12 @@
+function 여행(){
+    console.log("1. 계획을 세운다.")
+    console.log("2. 일정을 진행한다.")
+    console.log("3. 숙소에 복귀한다.")
+    console.log("4. 브이로그 편집을 한다.")
+ }
+ function 쉬다(){
+    console.log("1. 침대에 눕는다.")
+    console.log("2. 핸드폰을 충전기에 꽂는다.")
+    console.log("3. 유튜브를 본다.")
+    console.log("4. 잠든다.")    
+ }
