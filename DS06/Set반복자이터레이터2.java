cc346b910f3bcec973ca9ec6063d7ba9c748cce1
ColdBrew <99588377+Quokka2@users.noreply.github.com>
2023-04-17 17:43:31 +0900

    DS 06

---
+++ DS06/Set반복자이터레이터2.java
@@ -0,0 +1,29 @@
+package 문자열;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Set반복자이터레이터2 {
+
+	public static void main(String[] args){
+		HashSet<Integer> money = new HashSet<>();
+		money.add(1000);
+		money.add(2000);
+		money.add(3000);
+		money.add(2000);
+		money.add(4000);
+		money.add(2000);
+		money.add(2000);
+		
+		Iterator<Integer> it = money.iterator();
+		for (int i = 0; i < money.size(); i++) {
+			System.out.println(it.next());
+		}
+		System.out.println("================");
+		Object[] list = money.toArray();
+		for (Object x : list) {
+			System.out.println(x);
+		}
+	}
+
+}
